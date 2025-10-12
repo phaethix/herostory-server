29f32e90913f21db5006f76b3de54f1b37e41838
huanyu.li <cn.hylee@gmail.com>
2025-10-12 20:16:48 +0800

    test: add unit test for user_login_cmd parsing

--- internal/handler/websocket_test.go
+++ internal/handler/websocket_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"herostory-server/internal/pb"
 	"net/http"
 	"testing"
 	"time"
@@ -8,9 +9,10 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 )
 
-func TestWebSocketConnection(t *testing.T) {
+func Test_WebSocketConnection(t *testing.T) {
 	dialer := websocket.Dialer{
 		HandshakeTimeout: 3 * time.Second,
 	}
@@ -22,4 +24,23 @@ func TestWebSocketConnection(t *testing.T) {
 
 	assert.Equal(t, http.StatusSwitchingProtocols, resp.StatusCode)
 	assert.NoError(t, conn.WriteMessage(websocket.TextMessage, []byte("Hello World")))
+}
+
+func Test_ParseUserLoginCmd(t *testing.T) {
+	expectedCmd := &pb.UserLoginCmd{
+		UserName: "test_player",
+		Password: "test_pass123",
+	}
+
+	protoData, err := proto.Marshal(expectedCmd)
+	require.NoError(t, err)
+
+	fullData := append([]byte{0x00, 0x01, 0x02, 0x03}, protoData...)
+
+	cmd := &pb.UserLoginCmd{}
+	err = proto.Unmarshal(fullData[4:], cmd)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedCmd.UserName, cmd.UserName)
+	assert.Equal(t, expectedCmd.Password, cmd.Password)
 }
